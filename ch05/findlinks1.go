df63b83f8eb65cca9dc7eecc91085a09f25bce02
uh-zz <uhzz.contact@gmail.com>
2021-11-28 17:11:03 +0900

    :+1: Sample 5 recieve html from standard input and extract links

---
+++ ch05/findlinks1.go
@@ -0,0 +1,38 @@
+// findlinks1 は標準入力から読み込まれたHTMLドキュメント内のリンクを表示する
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"golang.org/x/net/html"
+)
+
+func main() {
+	doc, err := html.Parse(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "findlinks1: %v\n", err)
+		os.Exit(1)
+	}
+
+	for _, link := range visit(nil, doc) {
+		fmt.Println(link)
+	}
+
+}
+
+// visit は、n内で見つかったリンクを一つ一つlinksへ追加し、その結果を返す
+func visit(links []string, n *html.Node) []string {
+	if n.Type == html.ElementNode && n.Data == "a" {
+		for _, a := range n.Attr {
+			if a.Key == "href" {
+				links = append(links, a.Val)
+			}
+		}
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		links = visit(links, c)
+	}
+	return links
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/uh-zz/gpl
 
 go 1.16
+
+require golang.org/x/net v0.0.0-20211123203042-d83791d6bcd9
